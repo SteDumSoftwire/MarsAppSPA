31ac4662ea9836f8f5a1e2d2758c2df59b83f148
Stefan Dumitriu <Stefan.Dumitriu@softwire.com>
2022-07-05 16:27:58 +0300

    Responsive Navbar + Loading Animation

--- marsappspa/src/App.tsx
+++ marsappspa/src/App.tsx
@@ -11,43 +11,22 @@ import HomePage from "./HomePage";
 import Box from "@mui/material/Box";
 import AppBar from "@mui/material/AppBar";
 import Button from "@mui/material/Button";
-import Toolbar from "@mui/material/Toolbar";
-import {Grid, Link as MUILink} from '@mui/material/'
+import {Drawer, Grid, IconButton, Link as MUILink, List, ListItem, ListItemText, useMediaQuery, useTheme} from '@mui/material/'
 import RoverPage from "./RoverPage";
+import { useState } from "react";
+import 'material-icons/iconfont/material-icons.css';
+
 
 function App() {
+  const theme = useTheme();
+  const renderBurger = useMediaQuery(theme.breakpoints.down('md'));
+
   return (
     <Router>
         <Box sx={{ flexGrow: 1}}>
-          <AppBar position="static" color="primary">
-            <Grid container justifyContent="space-evenly" alignItems="center">
-              <Grid item xs={2}>
-                <Button color="inherit">
-                  <MUILink href="/" underline="none" color="white">Home</MUILink>
-                </Button>
-              </Grid>
-              <Grid item xs={2}>
-                <Button color="inherit">
-                  <MUILink href="/counter" underline="none" color="white">Counter</MUILink>
-                </Button>
-              </Grid>
-              <Grid item xs={2}>
-                <Button color="inherit">
-                  <MUILink href="/component" underline="none" color="white">My Component</MUILink>
-                </Button>
-              </Grid>
-              <Grid item xs={2}>
-                <Button color="inherit">
-                  <MUILink href="/component/complex" underline="none" color="white">Complex Component</MUILink>
-                </Button>
-              </Grid>
-              <Grid item xs={2}>
-                <Button color="inherit">
-                  <MUILink href="/rover" underline="none" color="white">Rovers</MUILink>
-                </Button>
-              </Grid>
-            </Grid>
-          </AppBar>
+          {
+            renderBurger? <DrawerComponent /> : <NavBar />
+          }
         </Box>
         <Routes>
           <Route path="" element={<HomePage />}/>
@@ -60,6 +39,85 @@ function App() {
     </Router>
   );
 }
+
+function NavBar() {
+  return(
+    <div>
+      <AppBar position="static" color="primary">
+        <Grid container justifyContent="space-evenly" alignItems="center" textAlign="center">
+          <Grid item xs={2}>
+            <Button color="inherit">
+              <MUILink href="/" underline="none" color="white">Home</MUILink>
+            </Button>
+          </Grid>
+          <Grid item xs={2}>
+            <Button color="inherit">
+              <MUILink href="/counter" underline="none" color="white">Counter</MUILink>
+            </Button>
+          </Grid>
+          <Grid item xs={2}>
+            <Button color="inherit">
+              <MUILink href="/component" underline="none" color="white">My Component</MUILink>
+            </Button>
+          </Grid>
+          <Grid item xs={2}>
+            <Button color="inherit">
+              <MUILink href="/component/complex" underline="none" color="white">Complex Component</MUILink>
+            </Button>
+          </Grid>
+          <Grid item xs={2}>
+            <Button color="inherit">
+              <MUILink href="/rover" underline="none" color="white">Rovers</MUILink>
+            </Button>
+          </Grid>
+        </Grid>
+      </AppBar>
+    </div>
+  )
+}
+
+function DrawerComponent() {
+    const [openDrawer, setOpenDrawer] = useState(false);
+    return (
+      <div style={{backgroundColor: "#1976d2"}}>
+        <Drawer
+          open={openDrawer}
+          onClose={() => setOpenDrawer(false)}
+        >
+        <Grid container justifyContent="space-evenly" alignItems="stretch" textAlign="center" direction="column" sx={{height: '50%'}}>
+          <Grid item xs={2}>
+            <Button color="inherit">
+              <MUILink href="/" underline="none" color="white">Home</MUILink>
+            </Button>
+          </Grid>
+          <Grid item xs={2}>
+            <Button color="inherit">
+              <MUILink href="/counter" underline="none" color="white">Counter</MUILink>
+            </Button>
+          </Grid>
+          <Grid item xs={2}>
+            <Button color="inherit">
+              <MUILink href="/component" underline="none" color="white">My Component</MUILink>
+            </Button>
+          </Grid>
+          <Grid item xs={2}>
+            <Button color="inherit">
+              <MUILink href="/component/complex" underline="none" color="white">Complex Component</MUILink>
+            </Button>
+          </Grid>
+          <Grid item xs={2}>
+            <Button color="inherit">
+              <MUILink href="/rover" underline="none" color="white">Rovers</MUILink>
+            </Button>
+          </Grid>
+        </Grid>
+        </Drawer>
+        <IconButton onClick={() => setOpenDrawer(!openDrawer)}>
+          <span className="material-icons">menu</span>
+        </IconButton>
+      </div>
+    );
+  }
 
 export default App;
 
--- marsappspa/src/RoverPage.tsx
+++ marsappspa/src/RoverPage.tsx
@@ -2,6 +2,7 @@ import { Button, FormControl, Grid, ImageList, ImageListItem, InputLabel, Menu, 
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.scss'
+import { Circles, Rings } from 'react-loader-spinner';
 
 function RoverPage() {
     return(
@@ -24,6 +25,7 @@ function SelectRover() {
 
     const [selectedRover, setSelectedRover] = useState('');
     const [selectedCamera, setSelectedCamera] = useState('');
+    const [loading, setLoading] = useState(false);
 
     const roverChange = (event: SelectChangeEvent) => {
         setSelectedRover(event.target.value);
@@ -36,10 +38,12 @@ function SelectRover() {
 
     const handleSubmit = async (e: any) => {
         e.preventDefault();
+        setLoading(true);
         await axios.get('http://localhost:8000/rovers/' + selectedRover.toLowerCase() + '/camera/' + selectedCamera + '?paginationStart=1&paginationEnd=1').then(
             (res) => {
                 setPhotos(res.data);
                 setWarning(false);
+                setLoading(false);
             }
         ).catch(err => {
             console.log(err)
@@ -50,9 +54,11 @@ function SelectRover() {
 
     useEffect(
         () => {
+            setLoading(true);
             axios.get('http://localhost:8000/rovers').then(
                 (res) => {
                     setRovers(res.data.rovers);
+                    setLoading(false);
                 }
             ).catch(err => console.log(err))
         }, []
@@ -60,9 +66,11 @@ function SelectRover() {
     useEffect(
         () => {
             const fetchCameras = async () => {
+                setLoading(true);
                 await axios.get('http://localhost:8000/rovers/' + selectedRover.toLowerCase()).then(
                     (res) => {
                         setCameras(res.data.rover.cameras);
+                        setLoading(false);
                     }
                 ).catch(err => console.log(err));
             }
@@ -76,34 +84,39 @@ function SelectRover() {
     return(
         <Grid container spacing={2}>
             <Grid item xs = {12}>
-            <FormControl>
-                <div className="Container">
-                    <Select value={selectedRover} onChange={roverChange} className="SelectField ContainerItem">
-                        {rovers.map((rover) => {
-                            return <MenuItem value={rover.name}>{rover.name}</MenuItem>
-                        })}
-                    </Select>
-                    <Select value={selectedCamera} onChange={cameraChange} className="SelectField ContainerItem">
-                        {cameras.map((camera) => {
-                            return <MenuItem value={camera.name}>{camera.full_name}</MenuItem>
-                        })}
-                    </Select>
-                    <Button variant='outlined' type='submit' fullWidth color="primary" onClick={handleSubmit} disabled={!selectedRover || !selectedCamera} className="ContainerItem SubmitButton">
-                        Get Photos
-                    </Button>
-                    {warning && <p className='ContainerItem'>{error}</p>}
-                </div>
-            </FormControl>
+                <FormControl>
+                    <div className="Container">
+                        {loading && 
+                            <div className="ContainerItem">
+                                <Rings height={120} width={120} color="#1976d2"/>
+                            </div>
+                        }
+                        <Select value={selectedRover} onChange={roverChange} className="SelectField ContainerItem" fullWidth>
+                            {rovers.map((rover) => {
+                                return <MenuItem value={rover.name}>{rover.name}</MenuItem>
+                            })}
+                        </Select>
+                        <Select value={selectedCamera} onChange={cameraChange} className="SelectField ContainerItem" fullWidth>
+                            {cameras.map((camera) => {
+                                return <MenuItem value={camera.name}>{camera.full_name}</MenuItem>
+                            })}
+                        </Select>
+                        <Button variant='outlined' type='submit' fullWidth color="primary" onClick={handleSubmit} disabled={!selectedRover || !selectedCamera} className="ContainerItem SubmitButton">
+                            Get Photos
+                        </Button>
+                        {warning && <p className='ContainerItem'>{error}</p>}
+                    </div>
+                </FormControl>
             </Grid>
             <Grid item xs = {8} sm= {10} sx={{margin: "auto"}}>
-                <ImageList cols={5} rowHeight={164}>
+                <ImageList cols={5} rowHeight={400}>
                     {photos.map((item) => (
                         <ImageListItem key={item.img_src}>
-                        <img
-                            src={`${item.img_src}?fit=crop&auto=format`}
-                            srcSet={`${item.img_src}?fit=crop&auto=format`}
-                            loading="lazy"
-                        />
+                            <img
+                                src={`${item.img_src}?w=400?h=400`}
+                                srcSet={`${item.img_src}`}
+                                loading="lazy"
+                            />
                         </ImageListItem>
                     ))}
                 </ImageList>
