b8d3226f42fef366f31c8065edda84502fd2e461
Stefan Dumitriu <Stefan.Dumitriu@softwire.com>
2022-07-05 13:38:08 +0300

    Integrated the SPA app with the API + responsive rover photos page

--- marsappspa/src/App.tsx
+++ marsappspa/src/App.tsx
@@ -12,27 +12,41 @@ import Box from "@mui/material/Box";
 import AppBar from "@mui/material/AppBar";
 import Button from "@mui/material/Button";
 import Toolbar from "@mui/material/Toolbar";
-import {Link as MUILink} from '@mui/material/'
+import {Grid, Link as MUILink} from '@mui/material/'
+import RoverPage from "./RoverPage";
 
 function App() {
   return (
     <Router>
         <Box sx={{ flexGrow: 1}}>
           <AppBar position="static" color="primary">
-            <Toolbar sx={{ display: "flex"}}>
-              <Button color="inherit" sx={{flexGrow: 1}}>
-                <MUILink href="/" underline="none" color="white">Home</MUILink>
-              </Button>
-              <Button color="inherit" sx={{flexGrow: 1}}>
-                <MUILink href="/counter" underline="none" color="white">Counter</MUILink>
-              </Button>
-              <Button color="inherit" sx={{flexGrow: 1}}>
-                <MUILink href="/component" underline="none" color="white">My Component</MUILink>
-              </Button>
-              <Button color="inherit" sx={{flexGrow: 1}}>
-                <MUILink href="/component/complex" underline="none" color="white">Complex Component</MUILink>
-              </Button>
-            </Toolbar>
+            <Grid container justifyContent="space-evenly" alignItems="center">
+              <Grid item xs={2}>
+                <Button color="inherit">
+                  <MUILink href="/" underline="none" color="white">Home</MUILink>
+                </Button>
+              </Grid>
+              <Grid item xs={2}>
+                <Button color="inherit">
+                  <MUILink href="/counter" underline="none" color="white">Counter</MUILink>
+                </Button>
+              </Grid>
+              <Grid item xs={2}>
+                <Button color="inherit">
+                  <MUILink href="/component" underline="none" color="white">My Component</MUILink>
+                </Button>
+              </Grid>
+              <Grid item xs={2}>
+                <Button color="inherit">
+                  <MUILink href="/component/complex" underline="none" color="white">Complex Component</MUILink>
+                </Button>
+              </Grid>
+              <Grid item xs={2}>
+                <Button color="inherit">
+                  <MUILink href="/rover" underline="none" color="white">Rovers</MUILink>
+                </Button>
+              </Grid>
+            </Grid>
           </AppBar>
         </Box>
         <Routes>
@@ -41,6 +55,7 @@ function App() {
           <Route path="component" element={<ComponentPage />}>
             <Route path="complex" element={<ComplexTreePage />}/>
           </Route>
+          <Route path="rover" element={<RoverPage />} /> 
         </Routes>
     </Router>
   );
